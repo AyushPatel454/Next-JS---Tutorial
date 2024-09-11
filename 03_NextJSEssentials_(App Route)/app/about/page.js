147bd8c581d56ace58ce72ba131bec989aeaefe2
Ayush Patel <aayushpatel25190@gmail.com>
2024-09-11 10:38:08 +0530

    Adding another route via the File system.
    - Create a new route name it: about (localhost:3000/about)
    - For this we need to creat a new about folder inside the app folder.
    - Inside about folder create a new file name it: page.js
    - Inside page.js: we will write the content of about page.
    
    -> File Name Matters:
    - Next JS relies on reserved, special filenames but the filename only matter inside 'app' folder.
    - page.js ---------> Define Page content.
    - layout.js -------> Define wrapper around page.
    - not-found.js ----> Define 'Not Found' fallback page.
    - error.js --------> Define 'Error' fallback page.

---
+++ 03_NextJSEssentials_(App Route)/app/about/page.js
@@ -0,0 +1,8 @@
+// page.js: It's define the page content for the about route.
+export default function AboutUsPage() {
+    return (
+        <main>
+            <h1>About Us</h1>
+        </main>
+    )
+}
